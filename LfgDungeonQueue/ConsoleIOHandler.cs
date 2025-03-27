fbff4fc1857506fe8cfcae822666cf97472bcf7f
Rey Obejero <rey_obejero@dlsu.edu.ph>
2025-03-27 18:22:21 +0800

    feat: Fetch dungeon queue parameters from console input

---
+++ LfgDungeonQueue/ConsoleIOHandler.cs
@@ -0,0 +1,22 @@
+namespace LfgDungeonQueue {
+
+    public class ConsoleIOHandler {
+        public static T PromptInput<T>(string message) {
+            while (true) {
+                Console.Write($"{message}: ");
+                string? input = Console.ReadLine()?.Trim();
+                
+                if (string.IsNullOrWhiteSpace(input)) {
+                    Console.WriteLine("Input cannot be empty.");
+                    continue;
+                }
+                
+                try {
+                    return (T)Convert.ChangeType(input, typeof(T));
+                } catch {
+                    Console.WriteLine("Input is invalid.");
+                }
+            }
+        }
+    }
+}
--- LfgDungeonQueue/Program.cs
+++ LfgDungeonQueue/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LfgDungeonQueue;
+
+int maxDungeonInstances = ConsoleIOHandler.PromptInput<int>("Maximum number of dungeons");
+int numTankPlayers = ConsoleIOHandler.PromptInput<int>("Number of tank players");
+int numHealerPlayers = ConsoleIOHandler.PromptInput<int>("Number of healer players");
+int numDpsPlayers = ConsoleIOHandler.PromptInput<int>("Number of DPS players");
+int minDungeonInstanceDuration = ConsoleIOHandler.PromptInput<int>("Minimum dungeon duration");
+int maxDungeonInstanceDuration = ConsoleIOHandler.PromptInput<int>("Maximum dungeon duration");
